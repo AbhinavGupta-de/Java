7f445de4d3a0607ca204fb8c4522c82b63580fd1
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-09-13 17:12:19 +0530

    Two Out Three Common Elements Problem Solved

--- HashSets_And_HasMaps/TwoOutOfThree.java
+++ HashSets_And_HasMaps/TwoOutOfThree.java
@@ -1,14 +1,47 @@
 package HashSets_And_HasMaps;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 
 public class TwoOutOfThree {
 
  public ArrayList<Integer> commonElements(ArrayList<Integer> A, ArrayList<Integer> B) {
+  HashSet<Integer> first = new HashSet<>(A);
+  HashSet<Integer> second = new HashSet<>(B);
 
+  ArrayList<Integer> result = new ArrayList<>();
+
+  for (Integer i : first) {
+   if (second.contains(i)) {
+    result.add(i);
+   }
+  }
+
+  return result;
  }
 
  public ArrayList<Integer> solve(ArrayList<Integer> A, ArrayList<Integer> B, ArrayList<Integer> C) {
+  ArrayList<Integer> first = commonElements(A, B);
+  ArrayList<Integer> second = commonElements(B, C);
+  ArrayList<Integer> third = commonElements(A, C);
 
+  HashSet<Integer> result = new HashSet<>();
+
+  for (Integer i : first) {
+   result.add(i);
+  }
+
+  for (Integer i : second) {
+   result.add(i);
+  }
+
+  for (Integer i : third) {
+   result.add(i);
+  }
+
+  ArrayList<Integer> finalResult = new ArrayList<>(result);
+  Collections.sort(finalResult);
+  return finalResult;
  }
 }
