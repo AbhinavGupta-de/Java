82a7a5878f778d7cc59aadf0427efcd98a26d36b
AbhinavGupta-de <abhinav.gupta@outlook.de>
2023-10-05 15:21:40 +0530

    mereged two sorted arrays

--- sorting/Merge2SortedArray.java
+++ sorting/Merge2SortedArray.java
@@ -3,9 +3,12 @@ package sorting;
 public class Merge2SortedArray {
  // Given two storted array merge both of them and return the sorted array
  public static int[] merge2SortedArray(int[] arr1, int[] arr2) {
+
   int[] arr = new int[arr1.length + arr2.length];
   int i = 0, j = 0, k = 0;
+
   while (i < arr1.length && j < arr2.length) {
+
    if (arr1[i] < arr2[j]) {
     arr[k] = arr1[i];
     i++;
@@ -15,6 +18,7 @@ public class Merge2SortedArray {
    }
    k++;
   }
+
   while (i < arr1.length) {
    arr[k] = arr1[i];
    i++;
---
+++ sorting/MergeTwoSortedSubArrays.java
@@ -0,0 +1,61 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class MergeTwoSortedSubArrays {
+
+ // We are given an array along with l, m and r.
+ public static int[] sort(int[] arr, int l, int m, int r) {
+
+  int[] result = new int[arr.length];
+
+  for (int i = 0; i < l; i++) {
+   result[i] = arr[i];
+  }
+  // Sub Array from l to m is sorted and array from m + 1 to r is also sorted we
+  // have to merge them.
+  int i = l;
+  int j = m + 1;
+  int k = l;
+  while (i <= m && j <= r) {
+   if (arr[i] < arr[j]) {
+    result[k] = arr[i];
+    i++;
+   } else {
+    result[k] = arr[j];
+    j++;
+   }
+   k++;
+  }
+
+  if (i > m) {
+   while (j <= r) {
+    result[k] = arr[j];
+    j++;
+    k++;
+   }
+  } else {
+   while (i <= m) {
+    result[k] = arr[i];
+    i++;
+    k++;
+   }
+  }
+
+  for (int d = r + 1; d < arr.length; d++) {
+   result[d] = arr[d];
+  }
+
+  return result;
+ }
+
+ public static void main(String[] args) {
+  int[] nums = { 8, 1, 3, 6, 11, 2, 4, 9, 7, 6 };
+  int l = 2;
+  int m = 4;
+  int r = 7;
+  int[] result = sort(nums, l, m, r);
+  System.out.println("Sorted Array: " + Arrays.toString(result));
+ }
+
+}
