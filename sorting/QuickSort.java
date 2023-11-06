fe4cac3cd13109526fde64b53b6b8b7bca789b50
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-11-06 10:32:48 +0530

    Implemented quick sort

---
+++ sorting/QuickSort.java
@@ -0,0 +1,51 @@
+package sorting;
+
+public class QuickSort {
+
+ public void sort(int[] arr) {
+  sortArray(arr, 0, arr.length - 1);
+ }
+
+ public void sortArray(int[] arr, int start, int end) {
+  if (start <= end) {
+   int pivot = partition(arr, start, end);
+   sortArray(arr, start, pivot - 1);
+   sortArray(arr, pivot + 1, end);
+  }
+ }
+
+ public int partition(int[] arr, int start, int end) {
+  int lastElement = arr[end];
+  int i = start;
+  int j = start;
+
+  while (i < end) {
+   if (arr[i] < lastElement) {
+    swap(arr, i, j);
+    j++;
+   }
+
+   i++;
+  }
+
+  swap(arr, j, end);
+
+  return j;
+ }
+
+ public void swap(int[] arr, int i, int j) {
+  int temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+ }
+
+ public static void main(String[] args) {
+  int[] arr = { 5, 4, 3, 2, 1 };
+  QuickSort qs = new QuickSort();
+  qs.sort(arr);
+  for (int i : arr) {
+   System.out.print(i + " ");
+  }
+ }
+
+}
