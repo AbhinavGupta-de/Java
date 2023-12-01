0f5272ceaa7e0db4caeae5b7097f9a143afe7133
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-12-01 09:51:27 +0530

    solved difference

---
+++ twoPointers/Difference.java
@@ -0,0 +1,25 @@
+package twoPointers;
+
+/**
+ * Difference
+ */
+public class Difference {
+
+ public boolean diffExist(int[] A, int B) {
+
+  int i = 0;
+  int j = 1;
+
+  while (i < A.length && j < A.length) {
+   if (i != j && A[j] - A[i] == B)
+    return true;
+   else if (A[j] - A[i] < B)
+    j++;
+   else
+    i++;
+  }
+
+  return false;
+ }
+
+}
