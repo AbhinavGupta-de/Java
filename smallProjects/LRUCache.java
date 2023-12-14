1dc4aa905434ad4ead10d54d044f198477f61b6b
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-12-14 21:59:02 +0530

    Change package names to lowercase

---
+++ smallProjects/LRUCache.java
@@ -0,0 +1,18 @@
+package smallProjects;
+
+import java.util.HashMap;
+
+/**
+ * LRUCache
+ */
+public class LRUCache {
+
+ // First check if it is present in cache or not -> HashSet
+ // Check if the space is full or not -> just a space property
+ // If it is present return it
+ // it is not present and the space is not full just add it
+ //
+
+ HashMap<Object, Long> map1 = new HashMap<>();
+
+}
