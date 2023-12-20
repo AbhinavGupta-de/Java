cf43b7e9e2331636c8d1e17930502f50c2367ca7
AbhinavGupta-de <abhinav.gupta@outlook.de>
2023-12-20 13:12:16 +0530

    Refactor RemoveConsectiveDuplicates class to handle character values

--- stacks/RemoveConsectiveDuplicates.java
+++ stacks/RemoveConsectiveDuplicates.java
@@ -7,21 +7,41 @@ import java.util.*;
  */
 public class RemoveConsectiveDuplicates {
 
- Stack<Integer> stack = new Stack<>();
+ Stack<Character> stack = new Stack<>();
 
- public void push(int val) {
-  if (stack.peek() == val) {
-   stack.pop();
-  } else
+ public void push(char val) {
+  if (!stack.empty()) {
+   if (stack.peek() == val) {
+    stack.pop();
+   } else
+    stack.push(val);
+  } else {
    stack.push(val);
+  }
  }
 
  public void pop() {
   stack.pop();
  }
 
- public int peek() {
+ public char peek() {
   return stack.peek();
  }
 
+ public String toString() {
+  String result = "";
+  for (Object i : stack.toArray()) {
+   result += i;
+  }
+
+  return result;
+ }
+
+ public String removeDuplicates(String s) {
+  for (char i : s.toCharArray()) {
+   push(i);
+  }
+
+  return toString();
+ }
 }
