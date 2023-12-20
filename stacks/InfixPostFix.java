8ca3344b44c14514487d3f3cf6cb862cf1ef020d
AbhinavGupta-de <abhinav.gupta@outlook.de>
2023-12-20 17:11:56 +0530

    Solved infixPostfix question

---
+++ stacks/InfixPostFix.java
@@ -0,0 +1,50 @@
+package stacks;
+
+import java.util.Stack;
+
+public class InfixPostFix {
+ public int precedence(char c) {
+  if (c == '^')
+   return 3;
+  if (c == '*' || c == '/')
+   return 2;
+  if (c == '+' || c == '-')
+   return 1;
+
+  return -1;
+ }
+
+ public String infixToPostfix(String s) {
+
+  Stack<Character> stack = new Stack<>();
+  String result = "";
+
+  for (char c : s.toCharArray()) {
+   if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+    result += c;
+   } else if (c == '(') {
+    stack.push(c);
+   } else if (c == ')') {
+    while (!stack.empty() && stack.peek() != '(') {
+     result += stack.peek();
+     stack.pop();
+    }
+    if (!stack.empty())
+     stack.pop();
+   } else {
+    while (!stack.empty() && precedence(stack.peek()) >= precedence(c)) {
+     result += stack.peek();
+     stack.pop();
+    }
+    stack.push(c);
+   }
+  }
+
+  while (!stack.empty()) {
+   result += stack.peek();
+   stack.pop();
+  }
+  return result;
+ }
+
+}
