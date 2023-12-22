fba722ccbd6276bc5b478afafab7657a337eae5f
AbhinavGupta-de <abhinav.gupta@outlook.de>
2023-12-22 13:47:49 +0530

    Solved closest Min

---
+++ arrays/ClosestMin.java
@@ -0,0 +1,59 @@
+package arrays;
+
+import java.util.Stack;
+
+public class ClosestMin {
+
+ public static int[] closestMin(int[] nums) {
+  // n^2
+  // for (int i = 1; i < nums.length; i++) {
+  // if (nums[i - 1] < nums[i])
+  // result[i] = nums[i - 1];
+  // else {
+  // int j = i - 1;
+  // while (j >= 0) {
+  // if (result[j] < nums[i]) {
+  // result[i] = result[j];
+  // break;
+  // }
+  // j--;
+  // }
+  // }
+  // }
+
+  // return result;
+
+  int[] result = new int[nums.length];
+
+  Stack<Integer> stack = new Stack<>();
+  for (int i = 0; i < nums.length; i++) {
+   if (stack.isEmpty()) {
+    result[i] = -1;
+    stack.push(nums[i]);
+   } else {
+    while (!stack.isEmpty()) {
+     if (stack.peek() < nums[i]) {
+      result[i] = stack.peek();
+      stack.push(nums[i]);
+      break;
+     }
+     stack.pop();
+    }
+
+    if (stack.isEmpty()) {
+     result[i] = -1;
+     stack.push(nums[i]);
+    }
+   }
+  }
+  return result;
+ }
+
+ public static void main(String[] args) {
+  int[] nums = { 4, 1, 2, 5, 2, 8 };
+  int[] result = closestMin(nums);
+  for (int i : result) {
+   System.out.print(i + " ");
+  }
+ }
+}
--- stacks/MidElement.java
+++ stacks/MidElement.java
@@ -16,6 +16,7 @@ public class MidElement {
   }
 
   public Node head;
+  public Node tail;
   public Node mid;
   public int size;
 
@@ -27,59 +28,23 @@ public class MidElement {
 
   public void push(int value) {
    Node newNode = new Node(value);
-   newNode.next = this.head;
-   if (this.head != null)
-    this.head.prev = newNode;
-   this.head = newNode;
-   this.size++;
-
-   // Adjust the mid pointer when the size is 1 or even
-   if (size == 1) {
-    this.mid = this.head;
-   } else if (size % 2 == 0) {
-    this.mid = this.mid.prev;
+   if (this.head == null) {
+    this.head = newNode;
+    this.tail = newNode;
+    this.mid = newNode;
    }
   }
 
   public int pop() {
-   int val = this.head.value;
 
-   if (this.head.next != null)
-    this.head.next.prev = null;
-
-   this.head = this.head.next;
-   size--;
-
-   // Adjust the mid pointer when the size is odd
-   if (size % 2 == 1) {
-    this.mid = this.mid.next;
-   }
-
-   return val;
   }
 
   public int getMid() {
-   if (this.mid == null)
-    return -1;
-   return this.mid.value;
+
   }
 
   public void deleteMid() {
-   if (this.mid == null)
-    return;
 
-   // If mid is the head, update both head and mid
-   if (this.mid.prev == null) {
-    this.mid = this.mid.next;
-    this.head = this.head.next;
-    if (this.head != null) {
-     this.head.prev = null;
-    }
-   } else {
-    // Update pointers when mid is not the head
-    this.mid.prev.next = this.mid.next;
-    this.mid = this.mid.next;
-   }
   }
  }
 
