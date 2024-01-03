2a203839b3f7d4634ffe4217cc6f0d6b19b38bc6
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-01-03 12:37:37 +0530

    Implemented a queue using stacks

---
+++ queues/ImplementQueueUsingStack.java
@@ -0,0 +1,64 @@
+package queues;
+
+import java.util.*;
+
+public class ImplementQueueUsingStack {
+
+ private class Queue {
+  Stack<Integer> front = new Stack<>();
+  Stack<Integer> reverse = new Stack<>();
+
+  public Queue() {
+
+  }
+
+  public void enqueue(int x) {
+   front.push(x);
+  }
+
+  public int dequeue() {
+   if (reverse.isEmpty() && front.isEmpty())
+    return Integer.MAX_VALUE;
+
+   if (reverse.isEmpty()) {
+    while (!front.isEmpty()) {
+     reverse.push(front.pop());
+    }
+   }
+
+   return reverse.pop();
+  }
+
+  public int front() {
+
+   if (reverse.isEmpty() && front.isEmpty())
+    return Integer.MAX_VALUE;
+
+   if (reverse.isEmpty()) {
+    while (!front.isEmpty()) {
+     reverse.push(front.pop());
+    }
+   }
+
+   return reverse.pop();
+  }
+
+ }
+
+ public static void main(String[] args) {
+  ImplementQueueUsingStack QueueStack = new ImplementQueueUsingStack();
+  ImplementQueueUsingStack.Queue queue = QueueStack.new Queue();
+
+  queue.enqueue(0);
+  queue.enqueue(1);
+  queue.enqueue(2);
+  queue.enqueue(3);
+  queue.enqueue(4);
+  queue.enqueue(5);
+
+  System.out.println(queue.dequeue());
+  System.out.println(queue.front());
+
+ }
+
+}
