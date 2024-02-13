6a7812d296b1d497bc69a307d1f88c2891dd505d
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-02-13 12:14:50 +0530

    Added constructors

--- enums/Developers.java
+++ enums/Developers.java
@@ -1,7 +1,20 @@
 package enums;
 
 enum Status {
- Running, Failed, Pending, Success;
+ Running("Running... All good"),
+ Failed("Try again... Failed"),
+ Pending("Pending... Please wait for sometime..."),
+ Success("Done!");
+
+ private String message;
+
+ private Status(String message) {
+  this.message = message;
+ }
+
+ public String getMessage() {
+  return message;
+ }
 }
 
 public class Developers {
@@ -9,7 +22,7 @@ public class Developers {
  public static void main(String[] args) {
 
   Status s = Status.Running;
-  System.out.println(s);
+  System.out.println(s.getMessage());
 
   Status[] statuses = Status.values();
 
@@ -19,27 +32,27 @@ public class Developers {
 
   switch (s) {
    case Running:
-    System.out.println("Running... All good");
+    System.out.println(Status.Running.getMessage());
     break;
    case Failed:
-    System.out.println("Try again... Failed");
+    System.out.println(Status.Failed.getMessage());
     break;
    case Pending:
-    System.out.println("Pening... Please wait for sometime...");
+    System.out.println(Status.Pending.getMessage());
     break;
    default:
-    System.out.println("Done!");
+    System.out.println(Status.Success.getMessage());
     break;
   }
 
   if (s == Status.Failed) {
-   System.out.println("Try again... Failed");
+   System.out.println(Status.Failed.getMessage());
   } else if (s == Status.Pending) {
-   System.out.println("Pening... Please wait for sometime...");
+   System.out.println(Status.Pending.getMessage());
   } else if (s == Status.Running) {
-   System.out.println("Running... All good");
+   System.out.println(Status.Running.getMessage());
   } else {
-   System.out.println("Done!");
+   System.out.println(Status.Success.getMessage());
   }
 
  }
