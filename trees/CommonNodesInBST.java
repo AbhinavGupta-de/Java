69c5fb70ee8ff2461032324b2c473e456dd49a52
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-02-14 14:54:13 +0530

    Trying to solve common node in BST

---
+++ trees/CommonNodesInBST.java
@@ -0,0 +1,16 @@
+package trees;
+
+import java.util.HashMap;
+
+public class CommonNodesInBST {
+ public int common(TreeNode rootA, TreeNode rootB) {
+
+  HashMap<Integer, Integer> map = new HashMap<>();
+
+
+
+ }
+
+ public 
+
+}
