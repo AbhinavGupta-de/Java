92a0ced92ec255d3e0a3f255997826b7fead5ab7
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-02-19 10:08:56 +0530

    implemented inorder iteretavely

--- trees/InOrder.java
+++ trees/InOrder.java
@@ -1,6 +1,8 @@
 package trees;
 
 import java.util.ArrayList;
+import java.util.Stack;
+import java.util.List;
 
 public class InOrder {
  public void inOrderTriversal(TreeNode root) {
@@ -27,6 +29,32 @@ public class InOrder {
   inOrderTriversal(root.left, result);
   result.add((Integer) root.value);
   inOrderTriversal(root.right, result);
+ }
+
+ // InOrder without recurssion
+ public List<Integer> inOrderTraversalIteratevely(TreeNode root) {
+
+  if (root == null)
+   return new ArrayList<Integer>();
+
+  Stack<TreeNode> stack = new Stack<>();
+  List<Integer> list = new ArrayList<>();
+  TreeNode current = root;
+
+  while (current != null || !stack.isEmpty()) {
+   while (current != null) {
+    stack.push(current);
+    current = current.left;
+   }
+
+   current = stack.pop();
+   list.add((Integer) current.value);
+   current = current.right;
+
+  }
+
+  return list;
+
  }
 
 }
